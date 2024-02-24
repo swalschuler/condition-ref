011ae40ed8a8b52ea7b3cb66740efe5ed02806ed
Schuyler Alschuler <alschuler.s@gmail.com>
2024-02-24 00:02:39 -0800

    Adds support for condition rings

--- src/App.tsx
+++ src/App.tsx
@@ -14,20 +14,49 @@ import { AppShell, MantineProvider, Title } from "@mantine/core";
 // Map file names to 5e conditions
 const CONDITION_ATTACHMENT_NAMES: { [key: string]: ConditionName } = {
   blinded: "blinded",
+  "condition marker - blinded": "blinded",
+
   charmed: "charmed",
+  "condition marker - charmed": "charmed",
+
   deafened: "deafened",
+  "condition marker - deafened": "bldeafenedinded",
+
   exhaustion: "exhaustion",
+  "condition marker - exhaustion": "exhaustion",
+
   frightened: "frightened",
+  "condition marker - frightened": "frightened",
+
   grappled: "grappled",
+  "condition marker - grappled": "grappled",
+
   incapacitated: "incapacitated",
+  "condition marker - incapacitated": "incapacitated",
+
   invisible: "invisible",
+  "condition marker - invisible": "invisible",
+
   paralyzed: "paralyzed",
+  "condition marker - paralyzed": "paralyzed",
+
   petrified: "petrified",
+  "condition marker - petrified": "petrified",
+
   poisoned: "poisoned",
+  "condition marker - poisoned": "poisoned",
+
   prone: "prone",
+  "condition marker - prone": "prone",
+
   restrained: "restrained",
+  "condition marker - restrained": "restrained",
+
   stunned: "stunned",
+  "condition marker - stunned": "stunned",
+
   unconscious: "unconscious",
+  "condition marker - unconscious": "unconscious",
 };
 
 const updateConditions = (
