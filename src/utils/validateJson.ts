89d84ab1e5426eaf94fe66fb8fd92efcfd1e6022
Schuyler Alschuler <alschuler.s@gmail.com>
2024-02-24 18:45:57 -0800

    Adds schema validation for user input

--- src/utils/validateJson.ts
+++ src/utils/validateJson.ts
@@ -1,5 +1,7 @@
-// https://github.com/mantinedev/mantine/blob/master/packages/%40mantine/core/src/components/JsonInput/validate-json/validate-json.ts
+import Ajv, { JSONSchemaType } from "ajv";
+const ajv = new Ajv();
 
+// https://github.com/mantinedev/mantine/blob/master/packages/%40mantine/core/src/components/JsonInput/validate-json/validate-json.ts
 export default function validateJson(
   value: string,
   deserialize: typeof JSON.parse
@@ -9,9 +11,53 @@ export default function validateJson(
   }
 
   try {
-    deserialize(value);
-    return true;
+    const obj = deserialize(value);
+    return inputSchemaValidator(obj);
   } catch (e) {
     return false;
   }
 }
+
+export type InputData = {
+  fileName: string;
+  title: string;
+  url?: string | undefined;
+  conditionEffects: string[];
+};
+
+const schema: JSONSchemaType<InputData> = {
+  type: "object",
+  properties: {
+    fileName: {
+      type: "string",
+    },
+    title: {
+      type: "string",
+    },
+    url: {
+      type: "string",
+      nullable: true,
+    },
+    conditionEffects: {
+      type: "array",
+      items: {
+        type: "string",
+      },
+    },
+  },
+  required: ["fileName", "title", "conditionEffects"],
+};
+
+const inputSchemaValidator = ajv.compile(schema);
+
+// const data = {
+//   foo: 1,
+//   bar: "abc",
+// };
+
+// if (validate(data)) {
+//   // data is MyData here
+//   console.log(data.foo);
+// } else {
+//   console.log(validate.errors);
+// }
