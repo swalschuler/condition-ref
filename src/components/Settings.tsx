b8dea7c79b58a8fff03e4f9bea25364867306d61
Schuyler Alschuler <alschuler.s@gmail.com>
2024-02-25 16:45:38 -0800

    Adds modal to prevent accidental loss of work

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -12,7 +12,9 @@ import {
 import validateJson, { MetaData } from "../utils/validateJson";
 import OBR from "@owlbear-rodeo/sdk";
 import { METADATA_ID } from "../utils/constants";
-import { IconFile } from "@tabler/icons-react";
+import { IconDeviceFloppy } from "@tabler/icons-react";
+import SaveChangesModal from "./SaveChangesModal";
+import { useDisclosure } from "@mantine/hooks";
 
 const updateMetaData = (state: MetaData) => {
   return OBR.room.setMetadata({ [METADATA_ID]: { ...state } });
@@ -36,6 +38,9 @@ const Settings = ({
   setJsonValue: Dispatch<SetStateAction<string>>;
 }) => {
   const [oldJson, setOldJson] = useState("");
+  const [modalOpened, { open: openModal, close: closeModal }] =
+    useDisclosure(false);
+
   useEffect(() => setOldJson(jsonValue), [opened]);
 
   const isJsonValid = validateJson(jsonValue, JSON.parse);
@@ -65,7 +70,13 @@ const Settings = ({
   return (
     <Drawer.Root
       opened={opened}
-      onClose={close}
+      onClose={() => {
+        if (jsonValue !== oldJson) {
+          openModal();
+          return;
+        }
+        close();
+      }}
       position="right"
       scrollAreaComponent={ScrollArea.Autosize}
     >
@@ -82,6 +93,23 @@ const Settings = ({
           <Drawer.CloseButton />
         </Drawer.Header>
         <Drawer.Body>
+          <SaveChangesModal
+            discard={() => {
+              setJsonValue(oldJson);
+              close();
+            }}
+            save={
+              isButtonEnabled()
+                ? () => {
+                    updateMetaData(state).then(() => setOldJson(jsonValue));
+                    close();
+                  }
+                : undefined
+            }
+            opened={modalOpened}
+            open={openModal}
+            close={closeModal}
+          />
           <Title order={4}>5e Condition Markers.</Title>
           You can enable common condition markers here.
           <Group>
@@ -122,7 +150,7 @@ const Settings = ({
             <Button
               type="submit"
               fullWidth
-              leftSection={<IconFile size={14} />}
+              leftSection={<IconDeviceFloppy size={14} />}
               disabled={!isButtonEnabled()}
               onClick={() =>
                 updateMetaData(state).then(() => setOldJson(jsonValue))
