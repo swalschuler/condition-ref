e03e5a5287787d5096deb30363bbf4b3bb8cabcd
Schuyler Alschuler <alschuler.s@gmail.com>
2024-03-10 18:40:29 -0700

    Changes all links to something I control

--- src/utils/conditionData.ts
+++ src/utils/conditionData.ts
@@ -6,7 +6,7 @@ export type ConditionDataSingleton = {
 const CONDITION_DATA: ConditionDataSingleton[] = [
   {
     name: "blinded",
-    url: "https://5thsrd.org/rules/conditions/#blinded",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.7rnx50kx66d2",
     conditionEffects: [
       "You automatically fail any ability check that requires sight.",
       "You have disadvantage on attack rolls.",
@@ -15,7 +15,7 @@ const CONDITION_DATA: ConditionDataSingleton[] = [
   },
   {
     name: "charmed",
-    url: "https://5thsrd.org/rules/conditions/#charmed",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.f2rpazsafmzw",
     conditionEffects: [
       "You can't attack the charmer or target them with harmful abilities or magical effects.",
       "The charmer has advantage while interacting socially with you.",
@@ -23,19 +23,19 @@ const CONDITION_DATA: ConditionDataSingleton[] = [
   },
   {
     name: "deafened",
-    url: "https://5thsrd.org/rules/conditions/#deafened",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.13dfho217c7y",
     conditionEffects: [
       "You automatically fail any ability check that requires hearing.",
     ],
   },
   {
     name: "exhaustion",
-    url: "https://5thsrd.org/rules/conditions/#exhaustion",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.b178uw8zi9sa",
     conditionEffects: ["You are exhausted."],
   },
   {
     name: "frightened",
-    url: "https://5thsrd.org/rules/conditions/#frightened",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.fehr92620b7j",
     conditionEffects: [
       "You have disadvantage on ability checks and attack rolls while the source of fear is within line of site.",
       "You can't willingly move closer to the source of fear.",
@@ -43,7 +43,7 @@ const CONDITION_DATA: ConditionDataSingleton[] = [
   },
   {
     name: "grappled",
-    url: "https://5thsrd.org/rules/conditions/#grappled",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.c62dxdmxpbxb",
 
     conditionEffects: [
       "Your speed is 0.",
@@ -53,13 +53,13 @@ const CONDITION_DATA: ConditionDataSingleton[] = [
   },
   {
     name: "incapacitated",
-    url: "https://5thsrd.org/rules/conditions/#incapacitated",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.kqgiys74zc7l",
 
     conditionEffects: ["You can't take actions or reactions."],
   },
   {
     name: "invisible",
-    url: "https://5thsrd.org/rules/conditions/#invisible",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.2zv69451nebm",
 
     conditionEffects: [
       "You can't be seen without the aid of magic or a special sense.",
@@ -71,7 +71,7 @@ const CONDITION_DATA: ConditionDataSingleton[] = [
   },
   {
     name: "paralyzed",
-    url: "https://5thsrd.org/rules/conditions/#paralyzed",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.ya64k5r8t9a2",
 
     conditionEffects: [
       "You are incapacitated and can't move or speak.",
@@ -82,7 +82,7 @@ const CONDITION_DATA: ConditionDataSingleton[] = [
   },
   {
     name: "petrified",
-    url: "https://5thsrd.org/rules/conditions/#petrified",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.h6j361tzj87t",
 
     conditionEffects: [
       "You, and any nonmagical object you are wearing or carrying, are transformed into a solid inanimate substance.",
@@ -96,7 +96,7 @@ const CONDITION_DATA: ConditionDataSingleton[] = [
   },
   {
     name: "poisoned",
-    url: "https://5thsrd.org/rules/conditions/#poisoned",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.kpujrafv3jhb",
 
     conditionEffects: [
       "You have disadvantage on attack rolls and ability checks.",
@@ -104,7 +104,7 @@ const CONDITION_DATA: ConditionDataSingleton[] = [
   },
   {
     name: "prone",
-    url: "https://5thsrd.org/rules/conditions/#prone",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.5ka99dck3hay",
 
     conditionEffects: [
       "Your only movement option is to crawl.",
@@ -114,7 +114,7 @@ const CONDITION_DATA: ConditionDataSingleton[] = [
   },
   {
     name: "restrained",
-    url: "https://5thsrd.org/rules/conditions/#restrained",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.fis4alit80w4",
 
     conditionEffects: [
       "Your speed becomes 0.",
@@ -125,7 +125,7 @@ const CONDITION_DATA: ConditionDataSingleton[] = [
   },
   {
     name: "stunned",
-    url: "https://5thsrd.org/rules/conditions/#stunned",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.ua8n08hh92f7",
 
     conditionEffects: [
       "You are incapacitated, can't move, and can only speak falteringly.",
@@ -135,7 +135,7 @@ const CONDITION_DATA: ConditionDataSingleton[] = [
   },
   {
     name: "unconscious",
-    url: "https://5thsrd.org/rules/conditions/#unconscious",
+    url: "https://docs.google.com/document/d/1bTLIHSIIr3pqQAIB00Gx5w0Ofc47XG4NI1YTY_-TXTY/edit#heading=h.9fnrfm7fbsna",
     conditionEffects: [
       "You are incapacitated, can't move or speak, and are unaware of your surroundings.",
       "You drop whatever you are holding and fall prone.",
