59843da35503f1c1cc3d18c25c6fed52ccf1f7be
Schuyler Alschuler <alschuler.s@gmail.com>
2024-05-08 02:01:58 -0700

    Move functions from parsingHelper to index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,26 @@
-import OBR from "@owlbear-rodeo/sdk";
+import OBR, { Item } from "@owlbear-rodeo/sdk";
 import { SettingsData } from "../components/Settings";
+
+export const getUniqueConditions = (
+  itemsLocal: Item[],
+  fileToNameMap: {
+    [key: string]: string;
+  }
+) => {
+  const usedConditions: string[] = [];
+
+  // Could maybe optimize by filtering as I go
+  for (const item of itemsLocal) {
+    if (fileToNameMap.hasOwnProperty(item.name)) {
+      usedConditions.push(fileToNameMap[item.name]);
+    }
+  }
+
+  const uniqueConditions = usedConditions.filter(
+    (value, index, array) => array.indexOf(value) === index
+  );
+  return uniqueConditions;
+};
 
 export const broadcastState = (state: SettingsData) => {
   OBR.broadcast
